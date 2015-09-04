11b6f9a1704b45dcf0c44a553067950862a5e23a
mastinux <andrea.pantaleo.93@gmail.com>
2015-09-04 23:07:24 +0200

    message floating in traces generation system

---
+++ trunk/src/Tracer.h
@@ -0,0 +1,40 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef TRACER_H_
+#define TRACER_H_
+
+#include <omnetpp.h>
+#include <string.h>
+
+namespace cloudMGn {
+
+class Tracer : public cSimpleModule{
+//private:
+//    cMessage *event; // pointer to the event object which we'll use for timing
+//    cMessage *msg; // variable to remember the message until we send it back
+
+public:
+    Tracer();
+    virtual ~Tracer();
+
+protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+} /* namespace cloudMGn */
+
+#endif /* TRACER_H_ */
